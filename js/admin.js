a312d2803905570a86c859578df85d8b68cc79ff
Brutols <dtwo97@gmail.com>
2024-01-25 12:52:10 +0100

    edit product + better organization

--- js/admin.js
+++ js/admin.js
@@ -1,12 +1,6 @@
 import { createInput, createRow, createForm, createLi } from "./components.js";
 import { fetchRequest } from "./fetch.js";
-import {
-  addListeners,
-  deleteProduct,
-  generateData,
-  showProducts,
-  addProduct,
-} from "./helper.js";
+import { addListeners, generateData, getProductData } from "./helper.js";
 
 const ACCESSTOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmOWU5YmJkNWQxMjAwMTg5MGQ0NjQiLCJpYXQiOjE3MDYwMDgyMTksImV4cCI6MTcwNzIxNzgxOX0.T5qTK5UycN--6e2OLyGxuuJa8XsZXRt5gONKE9ODvUY";
@@ -24,13 +18,66 @@ btnNewProd.addEventListener("click", function (ev) {
   ev.preventDefault();
   formContainer.innerHTML = "";
   createForm(formContainer, {});
+
   const bntSubmitProd = document.querySelector("#btn-submit_prod");
+  
   bntSubmitProd.addEventListener("click", function (ev) {
     ev.preventDefault();
     const product = generateData();
-    fetchRequest({url: URL, key: ACCESSTOKEN, method: "POST", data: product});
+    fetchRequest({ url: URL, key: ACCESSTOKEN, method: "POST", data: product });
     formContainer.innerHTML = "";
-    showProducts(URL, ACCESSTOKEN, productList);
     alert("prodotto aggiunto con successo");
+    showProducts(URL, ACCESSTOKEN, productList);
   });
 });
+
+const showProducts = async () => {
+  const json = await fetchRequest({
+    url: URL,
+    key: ACCESSTOKEN,
+    method: "GET",
+  });
+  productList.innerHTML = "";
+  json.forEach((el) => {
+    createLi(productList, el.name, el._id);
+  });
+
+  const edit = document.querySelectorAll(".icon_edit");
+
+  addListeners(edit, async function (ev) {
+    const id = ev.target.closest(".products_list_item").id;
+    const formData = await getProductData(URL, ACCESSTOKEN, id);
+    formContainer.innerHTML = "";
+    createForm(formContainer, formData);
+
+    const bntSubmitProd = document.querySelector("#btn-submit_prod");
+
+    bntSubmitProd.addEventListener("click", function (ev) {
+      ev.preventDefault();
+      const editedProduct = generateData();
+      fetchRequest({
+        url: URL,
+        key: ACCESSTOKEN,
+        method: "PUT",
+        data: editedProduct,
+        id: id,
+      });
+      formContainer.innerHTML = "";
+      alert(`${editedProduct.name} modificato correttamente`);
+      showProducts();
+    });
+  });
+
+  const remove = document.querySelectorAll(".icon_delete");
+  addListeners(remove, async function (ev) {
+    const id = ev.target.closest(".products_list_item").id;
+    const deleteProduct = await fetchRequest({
+      url: URL,
+      key: ACCESSTOKEN,
+      method: "DELETE",
+      id: id,
+    });
+    alert(`${deleteProduct.name} eliminato correttamente`);
+    showProducts();
+  });
+};
--- js/helper.js
+++ js/helper.js
@@ -1,5 +1,4 @@
-import { createForm, createLi } from "./components.js";
-const formContainer = document.querySelector(".manage");
+import { fetchRequest } from "./fetch.js";
 
 export const generateData = () => {
   const prodName = document.querySelector("#input_name").value;
@@ -24,82 +23,9 @@ export const addListeners = (array, func) => {
   });
 };
 
-export const deleteProduct = async (url, key, id) => {
-  try {
-    const resp = await fetch(url + id, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${key}`,
-      },
-    });
-    const json = await resp.json();
-    alert(`${json.name} eliminato correttamente`);
-  } catch (error) {
-    alert(`error: ${error}`);
-  }
-};
-
-export const showProducts = async (url, key, list) => {
-  try {
-    const res = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${key}`,
-      },
-    });
-    const json = await res.json();
-    list.innerHTML = "";
-    json.forEach((el) => {
-      createLi(list, el.name, el._id);
-    });
-    const edit = document.querySelectorAll(".icon_edit");
-    addListeners(edit, async function (ev) {
-      const id = ev.target.closest(".products_list_item").id;
-      const formData = await getProductData(url, key, id);
-      formContainer.innerHTML = "";
-      createForm(formContainer, formData);
-      const bntSubmitProd = document.querySelector("#btn-submit_prod");
-      bntSubmitProd.addEventListener("click", function(ev) {
-        ev.preventDefault()
-        const editedProduct = generateData();
-        editProduct(url, key, id, editedProduct);
-        formContainer.innerHTML = "";
-        showProducts(url, key, list);
-      })
-    });
-    const remove = document.querySelectorAll(".icon_delete");
-    addListeners(remove, function (ev) {
-      const id = ev.target.closest(".products_list_item").id;
-      deleteProduct(url, key, id);
-      showProducts(url, key, list);
-    });
-  } catch (error) {
-    alert(`error: ${error}`);
-  }
-};
-
-export const addProduct = async (data, url, key) => {
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        Authorization: `Bearer ${key}`,
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error) {
-    alert(`error: ${error}`);
-  }
-};
-
 export const getProductData = async (url, key, id) => {
   try {
-    const resp = await fetch(url + id, {
-      headers: {
-        Authorization: `Bearer ${key}`,
-      },
-    });
-    const json = await resp.json();
+    const json = await fetchRequest({url: url, key: key, method: "GET", id: id})
     const dataObj = {
       name: json.name,
       desc: json.description,
@@ -108,23 +34,6 @@ export const getProductData = async (url, key, id) => {
       price: json.price,
     };
     return dataObj;
-  } catch (error) {
-    alert(`error: ${error}`);
-  }
-};
-
-export const editProduct = async (url, key, id, data) => {
-  try {
-    const resp = await fetch(url + id, {
-      method: "PUT",
-      body: JSON.stringify(data),
-      headers: {
-        Authorization: `Bearer ${key}`,
-        "Content-Type": "application/json",
-      },
-    });
-    const json = await resp.json();
-    alert(`${json.name} modificato correttamente`);
   } catch (error) {
     alert(`error: ${error}`);
   }
