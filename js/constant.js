b082343e76d2c760354279488e210e20a9a2c392
Brutols <dtwo97@gmail.com>
2024-03-24 12:09:12 +0100

    update accesstoken

--- js/constant.js
+++ js/constant.js
@@ -2,7 +2,7 @@
 // fetch
 
 export const ACCESSTOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmOWU5YmJkNWQxMjAwMTg5MGQ0NjQiLCJpYXQiOjE3MDk0NzYyMDAsImV4cCI6MTcxMDY4NTgwMH0.YeJD-StjZWn9oOh1EahV20sUB2hJwwICdl1EcrBpw5Q";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmOWU5YmJkNWQxMjAwMTg5MGQ0NjQiLCJpYXQiOjE3MTEyNzg0MjgsImV4cCI6MTcxMjQ4ODAyOH0.uHiSmLoK_x7Lb-ICSwlion_3cRzqAtNAwSMAakFuO94";
 export const URL = "https://striveschool-api.herokuapp.com/api/product/";
 
 // end fetch
